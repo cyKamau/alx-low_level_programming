7fcbcc2f485fc62a70000d7aa9d750d79f3307b6
cyKamau <cynthiamsokamau@gmail.com>
2022-09-15 05:40:26 -0700

    my tenth commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 * main - print if the number is positive, zero, or negative
 *
 * Description: using the main function
-* this program will print "0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+* this program will print "Programming is positive, zero, or negative
 * Return: 0
 */
 int main(void)
